ef295d92521b8e7d47ba79d5e6ad5776add60992
VasileNovac <117888286+VasileNovac@users.noreply.github.com>
2023-05-05 22:52:58 +0300

    Add files via upload

--- BusSensus/src/main/java/BusSensus/BusSensusController.java
+++ BusSensus/src/main/java/BusSensus/BusSensusController.java
@@ -27,7 +27,7 @@ public class BusSensusController {
 	private String ynumberBus = "" ;
 	private String yrouteBus = "" ;
 	private String ystationBus = "" ;
-	private String insUpd, sql ;
+	private String insUpd, sql, texterr ;
 	public static String numberbus, routebus, stationbus ;
 	public ArrayList<String> listNB ;
 	public ArrayList<String> listRB ;
@@ -39,14 +39,15 @@ public class BusSensusController {
 	public void initNB( Model model ) {
 		model.addAttribute("ymodul", "initNB") ;
 		model.addAttribute("erori", erori) ;
+		model.addAttribute("texterr", texterr) ;
 		InitNB initNB = new InitNB(ynumberBus) ;
 		model.addAttribute("initNB", initNB) ;
 	}
 
 	@PostMapping("/addInitNB")
 	public String addInitNB( @ModelAttribute InitNB initNB, Model model ) {
-
 		erori = false ;
+		texterr = "Exista in baza de date" ;
 //		initNBRepository.save(initNB) ;
 		
 		xconn = BusSensusApplication.conn ;
@@ -83,18 +84,59 @@ public class BusSensusController {
 
 	}
 
+	@PostMapping("/delInitNB")
+	public String delInitNB( @ModelAttribute InitNB initNB, Model model ) {
+		erori = false ;
+		texterr = "NU exista in baza de date" ;
+		
+		xconn = BusSensusApplication.conn ;
+		try {
+			stmt = xconn.createStatement();
+			sql = "SELECT numberbus FROM numberbus WHERE numberbus=?" ;
+			PreparedStatement sqlNB = xconn.prepareStatement(sql);
+			sqlNB.setString(1, initNB.getNumberBus());
+			ResultSet rs = sqlNB.executeQuery();
+			if ( rs.next()) {
+				insUpd = "DELETE FROM numberbus WHERE numberbus=?" ;
+				PreparedStatement iS = xconn.prepareStatement(insUpd);
+				iS.setString(1, initNB.getNumberBus());
+				iS.executeUpdate();
+				iS.close();
+				
+				stmt = xconn.createStatement(); 
+				ResultSet sql = stmt.executeQuery("SELECT numberbus FROM numberbus order by numberbus") ;
+				listNB = new ArrayList<String>() ;
+				while(sql.next()) {
+					InitNB initnb = new InitNB(sql.getString("numberbus")) ;
+					listNB.add(initnb.getNumberBus()) ;
+				}
+				ynumberBus = "" ;
+			} else {
+				ynumberBus = initNB.getNumberBus();
+				erori = true ;
+			}
+			
+		} catch(SQLException se) { 
+//	Handle errors for JDBC 
+			se.printStackTrace(); 
+		}
+		return "redirect:initnb" ;
+
+	}
+
 	@GetMapping("/initrb")
 	public void initRB( Model model ) {
 		model.addAttribute("ymodul", "initRB") ;
 		model.addAttribute("erori", erori) ;
+		model.addAttribute("texterr", texterr) ;
 		InitRB initRB = new InitRB(yrouteBus) ;
 		model.addAttribute("initRB", initRB) ;
 	}
 
 	@PostMapping("/addInitRB")
 	public String addInitRB( @ModelAttribute InitRB initRB, Model model ) {
-
 		erori = false ;
+		texterr = "Exista in baza de date" ;
 //		initRBRepository.save(initRB) ;
 		
 		xconn = BusSensusApplication.conn ;
@@ -131,10 +173,50 @@ public class BusSensusController {
 
 	}
 
+	@PostMapping("/delInitRB")
+	public String delInitRB( @ModelAttribute InitRB initRB, Model model ) {
+		erori = false ;
+		texterr = "Nu exista in baza de date" ;
+		
+		xconn = BusSensusApplication.conn ;
+		try {
+			stmt = xconn.createStatement();
+			sql = "SELECT routebus FROM routebus WHERE routebus=?" ;
+			PreparedStatement sqlNB = xconn.prepareStatement(sql);
+			sqlNB.setString(1, initRB.getRouteBus());
+			ResultSet rs = sqlNB.executeQuery();
+			if ( rs.next()) {
+				insUpd = "DELETE FROM routebus WHERE routebus=?" ;
+				PreparedStatement iS = xconn.prepareStatement(insUpd);
+				iS.setString(1, initRB.getRouteBus());
+				iS.executeUpdate();
+				iS.close();
+				
+				stmt = xconn.createStatement(); 
+				ResultSet sql = stmt.executeQuery("SELECT routebus FROM routebus order by routebus") ;
+				listRB = new ArrayList<String>() ;
+				while(sql.next()) {
+					InitRB initrb = new InitRB(sql.getString("routebus")) ;
+					listRB.add(initrb.getRouteBus()) ;
+				}
+				yrouteBus = "" ;
+			} else {
+				yrouteBus = initRB.getRouteBus();
+				erori = true ;
+			}
+		} catch(SQLException se) { 
+//	Handle errors for JDBC 
+			se.printStackTrace(); 
+		}
+		return "redirect:initrb" ;
+
+	}
+
 	@GetMapping("/initsb")
 	public void initSB( Model model ) {
 		model.addAttribute("ymodul", "initSB") ;
 		model.addAttribute("erori", erori) ;
+		model.addAttribute("texterr", texterr) ;
 		InitSB initSB = new InitSB(yrouteBus, ystationBus) ;
 		model.addAttribute("initSB", initSB) ;
 		model.addAttribute("rbList", listRB) ;
@@ -143,6 +225,7 @@ public class BusSensusController {
 	@PostMapping("/addInitSB")
 	public String addInitSB( @ModelAttribute InitSB initSB, Model model ) {
 		erori = false ;
+		texterr = "Exista in baza de date" ;
 //		initSBRepository.save(initSB) ;
 			
 		xconn = BusSensusApplication.conn ;
@@ -173,6 +256,47 @@ public class BusSensusController {
 					listSB.add(initsb) ;
 				}
 				ystationBus = "" ;
+			}
+		} catch(SQLException se) { 
+//	Handle errors for JDBC 
+			se.printStackTrace(); 
+		}
+		return "redirect:initsb" ;
+	}
+
+	@PostMapping("/delInitSB")
+	public String delInitSB( @ModelAttribute InitSB initSB, Model model ) {
+		erori = false ;
+		texterr = "Nu exista in baza de date" ;
+			
+		xconn = BusSensusApplication.conn ;
+		try {
+			stmt = xconn.createStatement();
+			sql = "SELECT routebus, stationbus FROM stationbus WHERE routebus=? AND stationbus=?" ;
+			PreparedStatement sqlNB = xconn.prepareStatement(sql);
+			sqlNB.setString(1, initSB.getRouteBus());
+			sqlNB.setString(2, initSB.getStationBus());
+			ResultSet rs = sqlNB.executeQuery();
+			if ( rs.next()) {
+				insUpd = "DELETE FROM stationbus WHERE routebus=? AND stationbus=?" ;
+				PreparedStatement iS = xconn.prepareStatement(insUpd);
+				iS.setString(1, initSB.getRouteBus());
+				iS.setString(2, initSB.getStationBus());
+				iS.executeUpdate();
+				iS.close();
+				yrouteBus = initSB.getRouteBus();
+				
+				stmt = xconn.createStatement(); 
+				ResultSet sql = stmt.executeQuery("SELECT routebus, stationbus FROM stationbus order by routebus") ;
+				listSB = new ArrayList<InitSB>() ;
+				while(sql.next()) {
+					InitSB initsb = new InitSB(sql.getString("routebus"), sql.getString("stationbus")) ;
+					listSB.add(initsb) ;
+				}
+				ystationBus = "" ;
+			} else {
+				yrouteBus = initSB.getRouteBus();
+				erori = true ;
 			}
 		} catch(SQLException se) { 
 //	Handle errors for JDBC 
